e70c2afa75cd7de9923ae38cb51e08c522633dce
JhonBoyaca <jhonboyaca99@gmail.com>
2023-03-23 08:58:06 -0600

    Update1

---
+++ Examen1DS39/Examen1DS39/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Examen1DS39.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
